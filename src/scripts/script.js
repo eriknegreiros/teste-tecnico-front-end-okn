4b82e0d0763bd73bc8c312b054e5ba9834a33815
Erik <eriksn07@gmail.com>
2023-03-10 00:09:40 -0300

    Feat: Page Built

---
+++ src/scripts/script.js
@@ -0,0 +1,24 @@
+    const video = document.getElementsByTagName('video')
+    const btnPlay = document.querySelector('.project-play-icon')
+    const content = document.querySelector('.project-content-video')
+    const btnPause = document.querySelector('.project-video-btn-pause')
+   
+    function videoPlay() {
+        btnPlay.addEventListener('click', () => {
+            video[0].play()
+            content.classList.add('none')
+            btnPause.classList.remove('none')
+        } )
+    }
+
+    function videoPause() {
+        btnPause.addEventListener('click', () => {
+            video[0].pause()
+            content.classList.remove('none')
+            btnPause.classList.add('none')
+        })
+    }
+   
+  
+  videoPlay()
+  videoPause()
