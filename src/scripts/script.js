b5130d81bef1df3528280248353492d06ac601e3
Erik <eriksn07@gmail.com>
2023-03-10 14:27:23 -0300

    Feat: Finished

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,24 @@
-    const video = document.getElementsByTagName('video')
-    const btnPlay = document.querySelector('.project-play-icon')
-    const content = document.querySelector('.project-content-video')
-    const btnPause = document.querySelector('.project-video-btn-pause')
-   
-    function videoPlay() {
-        btnPlay.addEventListener('click', () => {
-            video[0].play()
-            content.classList.add('none')
-            btnPause.classList.remove('none')
-        } )
-    }
+const video = document.getElementsByTagName('video')
+const btnPlay = document.querySelector('.project-play-icon')
+const content = document.querySelector('.project-content-video')
+const btnPause = document.querySelector('.project-video-btn-pause')
 
-    function videoPause() {
-        btnPause.addEventListener('click', () => {
-            video[0].pause()
-            content.classList.remove('none')
-            btnPause.classList.add('none')
-        })
-    }
-   
-  
-  videoPlay()
-  videoPause()
+function videoPlay() {
+    btnPlay.addEventListener('click', () => {
+        video[0].play()
+        content.classList.add('none')
+        btnPause.classList.remove('none')
+    })
+}
+
+function videoPause() {
+    btnPause.addEventListener('click', () => {
+        video[0].pause()
+        content.classList.remove('none')
+        btnPause.classList.add('none')
+    })
+}
+
+
+videoPlay()
+videoPause()
